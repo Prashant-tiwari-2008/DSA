79b2e893f131297c6e1764f9d2e3edf04109b98b
Prashant tiwari <prashanttiwari.vns@gmail.com>
2024-09-25 22:45:32 +0530

    notes added

--- algorithum/sorting/selectionSort/app.js
+++ algorithum/sorting/selectionSort/app.js
@@ -3,6 +3,23 @@ console.log("selection sort")
 let array = [6, 5, 7, 3, 8, 2, 1, 4]
 console.log("old array", array)
 
+function MySelectionSort(array) {
+    for(let i=0; i < array.length;i++){
+        let min = i;
+        for(let j=i; j < array.length; j++){
+            if(array[min] > array[j]){
+                min = j;
+            }
+        }
+       let temp = arr[min];
+       arr[min] = arr[i];
+       arr[i] = temp;
+    }
+    return array
+}
+
+
+// not good code 
 function mySelectionSort(array) {
     for(let i=0; i < array.length;i++){
         let min = array[i];
