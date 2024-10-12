449a0335f70990453262107e3fbfd1ab622a623d
Prashant tiwari <prashanttiwari.vns@gmail.com>
2024-10-12 13:50:16 +0530

    Queuq question added

--- LeetCode/75-Challange/Stack/Decode_String.js
+++ LeetCode/75-Challange/Stack/Decode_String.js
@@ -11,13 +11,68 @@ let s1 = "3[a2[c]]" // "accaccacc"
 let s2 = "2[abc]3[cd]ef" // "abcabccdcdcdef"
 
 var decodeString = function (s) {
-    let result = "";
-    let length = s.length - 1;
-    for (let i = length; i >= 0; i--) {
-        
+    let stack = [];
+    let length = s.length;
+    let result = '';
+    let i = 0;
+    while (i < length) {
+        if (s[i] == ']') {
+            let tempStr = '';
+            let secondTop = 0;
+            while (!isNaN(secondTop)) {
+                let topEle = stack.pop();
+                secondTop = stack.pop();
+                if(isNaN(secondTop)){
+                    tempStr += topEle + secondTop;
+                }else{
+                    tempStr += topEle.repeat(secondTop);
+                    secondTop = 'a'
+                }
+            }
+            result += tempStr;
+            i++;
+        } else if (s[i] !== '[') {
+            let newEle = isNaN(Number(s[i])) ? s[i] : Number(s[i])
+            stack.push(s[i])
+            i++;
+        } else {
+            i++;
+        }
     }
+    return result
 };
 
 console.log(decodeString(s))
-console.log(decodeString(s1))
-console.log(decodeString(s2))
+// console.log(decodeString(s1))
+// console.log(decodeString(s2))
+
+
+//chat_GPT solution
+var decodeString = function(s) {
+    let stack = [];
+    let currentNum = 0;
+    let currentStr = '';
+    
+    for (let i = 0; i < s.length; i++) {
+        if (!isNaN(s[i])) {
+            // Build the current number (to handle multi-digit numbers like 12, 123, etc.)
+            currentNum = currentNum * 10 + Number(s[i]);
+        } else if (s[i] === '[') {
+            // Push the current string and number onto the stack
+            stack.push(currentStr);
+            stack.push(currentNum);
+            currentStr = '';
+            currentNum = 0;
+        } else if (s[i] === ']') {
+            // Pop the number and previous string from the stack
+            let num = stack.pop();
+            let prevStr = stack.pop();
+            currentStr = prevStr + currentStr.repeat(num); // Build the decoded string
+        } else {
+            // Add the current character to the current string
+            currentStr += s[i];
+        }
+    }
+    
+    return currentStr;
+};
